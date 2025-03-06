e6139299c842cf1101e7b2b43bc62e0b7c5ddda5
Dean Kinyua <kinyuadean@gmail.com>
2025-03-06 12:58:21 +0300

    remove the default phoenix layout code in app.html.heex . introduce a liveview SkepticBotWeb.HomeLive.Index as our entry point liveview

---
+++ lib/skeptic_bot_web/live/home/home.ex
@@ -0,0 +1,26 @@
+defmodule SkepticBotWeb.HomeLive.Index do
+  @moduledoc """
+  The home page containing the chat input.
+  """
+
+  use SkepticBotWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="border border-red-400">
+      home
+    </div>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
+  end
+end
--- lib/skeptic_bot_web/router.ex
+++ lib/skeptic_bot_web/router.ex
@@ -18,6 +18,8 @@ defmodule SkepticBotWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/home", HomeLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
