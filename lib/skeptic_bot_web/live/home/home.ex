2e104dd0f99f9e4fd25833fb1ee27908bc7ad86f
Dean Kinyua <kinyuadean@gmail.com>
2025-03-08 14:40:23 +0300

    add absolute styles to all the custom elements that add aesthetics to the five pictures in our grid

--- lib/skeptic_bot_web/live/home/home.ex
+++ lib/skeptic_bot_web/live/home/home.ex
@@ -87,6 +87,11 @@ defmodule SkepticBotWeb.HomeLive.Index do
           </div>
         </section>
 
+        <img src={~p"/images/grid/vector1.svg"} alt="Vector 1" class="vector1" />
+
+        <img src={~p"/images/grid/vector2.svg"} alt="Vector 2" class="vector2" />
+
+        <img src={~p"/images/grid/vector3.svg"} alt="Vector 3" class="vector3" />
         <div class="image-title text-3xl">Autopilot</div>
       </section>
 
@@ -101,6 +106,12 @@ defmodule SkepticBotWeb.HomeLive.Index do
             <img src={~p"/images/media/youtube.svg"} alt="YouTube Logo" />
           </div>
         </section>
+
+        <img src={~p"/images/grid/star2.svg"} alt="Star 2" class="vector4" />
+        <img src={~p"/images/grid/vector5.svg"} alt="Vector 5" class="vector5" />
+
+        <img src={~p"/images/grid/vector4.svg"} alt="Vector 4" class="vector6" />
+
         <div class="image-title text-3xl">Self-confidence</div>
       </section>
 
@@ -115,6 +126,8 @@ defmodule SkepticBotWeb.HomeLive.Index do
             <img src={~p"/images/media/youtube.svg"} alt="YouTube Logo" />
           </div>
         </section>
+
+        <img src={~p"/images/grid/vector7.svg"} alt="Vector 7" class="vector7" />
         <div class="image-title text-3xl">Perplexed mind</div>
       </section>
 
@@ -129,6 +142,20 @@ defmodule SkepticBotWeb.HomeLive.Index do
             <img src={~p"/images/media/youtube.svg"} alt="YouTube Logo" />
           </div>
         </section>
+
+        <img src={~p"/images/grid/vector8.svg"} alt="Vector 8" class="vector8" />
+
+        <img src={~p"/images/grid/vector9.svg"} alt="Vector 9" class="vector9" />
+
+        <img src={~p"/images/grid/vector10.svg"} alt="Vector 10" class="vector10" />
+        <img src={~p"/images/grid/vector11.svg"} alt="Vector 11" class="vector11" />
+        <img src={~p"/images/grid/vector12.svg"} alt="Vector 12" class="vector12" />
+        <img src={~p"/images/grid/vector13.svg"} alt="Vector 13" class="vector13" />
+        <img src={~p"/images/grid/vector14.svg"} alt="Vector 14" class="vector14" />
+        <img src={~p"/images/grid/vector15.svg"} alt="Vector 15" class="vector15" />
+        <img src={~p"/images/grid/vector16.svg"} alt="Vector 16" class="vector16" />
+        <img src={~p"/images/grid/vector17.svg"} alt="Vector 17" class="vector17" />
+
         <div class="image-title text-3xl">Women's Rights</div>
       </section>
 
@@ -143,6 +170,14 @@ defmodule SkepticBotWeb.HomeLive.Index do
             <img src={~p"/images/media/youtube.svg"} alt="YouTube Logo" />
           </div>
         </section>
+
+        <img src={~p"/images/grid/vector18.svg"} alt="Vector 18" class="vector18" />
+        <img src={~p"/images/grid/vector19.svg"} alt="Vector 19" class="vector19" />
+        <img src={~p"/images/grid/vector20.svg"} alt="Vector 20" class="vector20" />
+        <img src={~p"/images/grid/vector21.svg"} alt="Vector 21" class="vector21" />
+        <img src={~p"/images/grid/vector22.svg"} alt="Vector 22" class="vector22" />
+        <img src={~p"/images/grid/vector23.svg"} alt="Vector 23" class="vector23" />
+
         <div class="image-title text-3xl">Social Class</div>
       </section>
     </div>
