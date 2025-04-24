eca19237b15cbc9165114a9d0a7776b02ea34d67
Dean Kinyua <kinyuadean@gmail.com>
2025-04-24 14:19:53 +0300

    change width

--- lib/skeptic_bot_web/live/home_live/index.ex
+++ lib/skeptic_bot_web/live/home_live/index.ex
@@ -5,12 +5,12 @@ defmodule SkepticBotWeb.HomeLive.Index do
   def render(assigns) do
     ~H"""
     <div class="bg-[#FFF5F5]">
-      <div class="flex items-center h-screen relative">
+      <div class="h-screen flex items-center relative">
         <section>
-          <div class="absolute top-[2%] left-0 2xl:top-[3%] w-[20%] xl:w-[22%] 2xl:w-[22%] 3xl:w-[21%] 4xl:w-[17%]">
+          <div class="absolute top-[2%] left-0 w-[20%] 2xl:top-[3%] 4xl:w-[17%]">
             <img src={~p"/images/home/top_swirl.svg"} class="w-full h-full object-cover" alt="Swirl" />
           </div>
-          <div class="absolute bottom-[20%] left-[4%] w-[20%] xl:w-[21%] 2xl:w-[22%] 3xl:w-[20%] 4xl:w-[17%]">
+          <div class="absolute bottom-[20%] left-[4%] w-[20%] xl:w-[21%] 2xl:w-[20%] 4xl:w-[17%]">
             <img
               src={~p"/images/home/demonstration.svg"}
               class="w-full h-full object-cover"
@@ -26,7 +26,7 @@ defmodule SkepticBotWeb.HomeLive.Index do
           </div>
         </section>
         <section class="flex flex-col gap-8 w-[70%] mx-auto">
-          <section class="text-7xl 2xl:text-8xl mx-auto montserrat-semibold tracking-4">
+          <section class="text-7xl mx-auto montserrat-semibold tracking-4 2xl:text-8xl">
             Skeptic.<span class="text-[#CD4631] montserrat-alternates-semibold">bot</span>
           </section>
           <section class="w-[50%] mx-auto text-center montserrat-alternates-medium text-[#4D4D4D]">
