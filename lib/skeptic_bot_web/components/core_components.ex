3ca30bd0bea58e3258ad27fa381dad9f4a8bc47b
Dean Kinyua <kinyuadean@gmail.com>
2025-06-09 14:14:09 +0300

    remove calls to undefined css classes

--- lib/skeptic_bot_web/components/core_components.ex
+++ lib/skeptic_bot_web/components/core_components.ex
@@ -232,7 +232,7 @@ defmodule SkepticBotWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg py-2 px-3",
+        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
         "text-sm font-semibold leading-6 text-white active:text-white/80",
         @class
       ]}
@@ -368,6 +368,7 @@ defmodule SkepticBotWeb.CoreComponents do
     """
   end
 
+  # All other inputs text, datetime-local, url, password, etc. are handled here...
   def input(assigns) do
     ~H"""
     <div phx-feedback-for={@name}>
@@ -378,9 +379,10 @@ defmodule SkepticBotWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "montserrat-alternates-medium placeholder:text-sm caret-[#0079FF] mono-400 w-full border-none bg-[#FFFFFF] outline-none rounded-lg py-[7px] px-[11px] remove-outline",
-          "placeholder:text-[#4D4D4D]  sm:text-lg sm:leading-6",
-          @errors != [] && "border-rose-400 focus:border-rose-400 focus:ring-rose-400/10"
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
         {@rest}
       />
