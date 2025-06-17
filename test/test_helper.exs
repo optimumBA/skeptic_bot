6bdca2f91f5ff08baec814e59ba8317bdf225d6a
Dean Kinyua <kinyuadean@gmail.com>
2025-06-17 14:16:40 +0300

    an episode does not have to have an embedding so that the rag process passes

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 Mox.defmock(SkepticBot.Rag.MockEmbedder, for: SkepticBot.Rag.Embedder)
-Mox.defmock(SkepticBot.Rag.MockGenerator, for: SkepticBot.Rag.Generator)
 Application.put_env(:skeptic_bot, :embedder, SkepticBot.Rag.MockEmbedder)
+Mox.defmock(SkepticBot.Rag.MockGenerator, for: SkepticBot.Rag.Generator)
 Application.put_env(:skeptic_bot, :generator, SkepticBot.Rag.MockGenerator)
 
 ExUnit.start()
