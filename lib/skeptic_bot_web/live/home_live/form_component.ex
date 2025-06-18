c248e0c5248a2e2ed6f42570d7db2b36ae64a33d
Dean Kinyua <kinyuadean@gmail.com>
2025-06-18 12:58:20 +0300

    switch from using Task.start to using start_async

--- lib/skeptic_bot_web/live/home_live/form_component.ex
+++ lib/skeptic_bot_web/live/home_live/form_component.ex
@@ -1,27 +1,18 @@
 defmodule SkepticBotWeb.HomeLive.QuestionFormComponent do
   @moduledoc """
-  Our form component.
-  Forwads the user Prompt to the LLM.
+  Holds our form
   """
 
-  use SkepticBotWeb, :live_component
+  use SkepticBotWeb, :html
 
-  alias SkepticBot.Prompts
-  alias SkepticBot.Prompts.UserQuestion
-  alias SkepticBot.Rag
+  @type assigns :: map()
+  @type rendered :: Phoenix.LiveView.Rendered.t()
 
-  @type socket :: Phoenix.LiveView.Socket.t()
-  @impl Phoenix.LiveComponent
-  def render(assigns) do
+  @spec component(assigns()) :: rendered()
+  def component(assigns) do
     ~H"""
     <div>
-      <.form
-        for={@form}
-        phx-target={@myself}
-        phx-change="validate"
-        phx-submit="save"
-        id="prompt-input-form"
-      >
+      <.form for={@form} phx-change="validate" phx-submit="save" id="prompt-input-form">
         <div class="flex justify-between mt-8 items-center rounded-xl hover:cursor-pointer custom-shadow border bg-[#FFFFFF] py-2">
           <div class="w-2/3 grow pl-4">
             <.input
@@ -48,66 +39,5 @@ defmodule SkepticBotWeb.HomeLive.QuestionFormComponent do
       </.form>
     </div>
     """
-  end
-
-  @impl Phoenix.LiveComponent
-  def update(assigns, socket) do
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:question, %UserQuestion{})
-     |> assign_form()}
-  end
-
-  @impl Phoenix.LiveComponent
-  def handle_event(
-        "validate",
-        %{"prompt" => prompt_params},
-        %{assigns: %{question: question}} = socket
-      ) do
-    changeset =
-      question
-      |> Prompts.change_prompt_question(prompt_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign(socket, :form, to_form(changeset, as: "prompt"))}
-  end
-
-  def handle_event(
-        "save",
-        %{"prompt" => %{"query" => query} = prompt_params},
-        %{assigns: %{question: question}} = socket
-      ) do
-    changeset =
-      Prompts.change_prompt_question(question, prompt_params)
-
-    maybe_generate_prompt_results(changeset.valid?, query)
-
-    {:noreply, socket}
-  end
-
-  defp maybe_generate_prompt_results(false, _query) do
-    :ok
-  end
-
-  defp maybe_generate_prompt_results(true, query) do
-    caller = self()
-    send(caller, {:loading_state, true})
-
-    Task.start(fn ->
-      {:ok, {description, list_of_episodes}} = Rag.generate(query)
-
-      if list_of_episodes == [] do
-        send(caller, :no_episodes_found)
-        send(caller, {:loading_state, false})
-      else
-        send(caller, {:generation_done, {description, list_of_episodes, query}})
-      end
-    end)
-  end
-
-  @spec assign_form(socket()) :: socket()
-  def assign_form(%{assigns: %{question: question}} = socket) do
-    assign(socket, :form, to_form(Prompts.change_prompt_question(question), as: "prompt"))
   end
 end
