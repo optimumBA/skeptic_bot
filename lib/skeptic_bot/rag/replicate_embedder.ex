b88cc12a1d671341cccc817dfb8d22f31d7a1304
Dean Kinyua <kinyuadean@gmail.com>
2025-06-23 14:55:53 +0300

    use alias instead of whole module name

--- lib/skeptic_bot/rag/replicate_embedder.ex
+++ lib/skeptic_bot/rag/replicate_embedder.ex
@@ -10,7 +10,7 @@ defmodule SkepticBot.Rag.ReplicateEmbedder do
   require Logger
 
   @behaviour Embedder
-  @behaviour SkepticBot.ReplicateClient
+  @behaviour ReplicateClient
 
   @model "beautyyuyanli/multilingual-e5-large:a06276a89f1a902d5fc225a9ca32b6e8e6292b7f3b136518878da97c458e2bad"
 
