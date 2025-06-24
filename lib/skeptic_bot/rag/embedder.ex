c19b381e9c26a262261d1d0263759cccccfb5e8c
Dean Kinyua <kinyuadean@gmail.com>
2025-06-24 14:12:48 +0300

    assign the name reason to type any

--- lib/skeptic_bot/rag/embedder.ex
+++ lib/skeptic_bot/rag/embedder.ex
@@ -4,14 +4,15 @@ defmodule SkepticBot.Rag.Embedder do
   alias SkepticBot.Rag.ReplicateEmbedder
 
   @type embedding :: [float()]
+  @type reason :: any()
   @type text :: String.t()
 
-  @callback generate([text()]) :: {:ok, [embedding()]} | {:error, any()}
+  @callback generate([text()]) :: {:ok, [embedding()]} | {:error, reason()}
 
-  @spec generate(text()) :: {:ok, [embedding()]} | {:error, any()}
+  @spec generate(text()) :: {:ok, [embedding()]} | {:error, reason()}
   def generate(text) when is_binary(text), do: generate([text])
 
-  @spec generate([text()]) :: {:ok, [embedding()]} | {:error, any()}
+  @spec generate([text()]) :: {:ok, [embedding()]} | {:error, reason()}
   def generate(texts) when is_list(texts) do
     impl().generate(texts)
   end
