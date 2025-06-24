d8fcee58a17a1fd34f02c265ad7e0b308aeca815
Dean Kinyua <kinyuadean@gmail.com>
2025-06-24 13:23:05 +0300

    refine the prompts context

--- lib/skeptic_bot/prompts.ex
+++ lib/skeptic_bot/prompts.ex
@@ -1,6 +1,6 @@
 defmodule SkepticBot.Prompts do
   @moduledoc """
-  The context for our prompt
+  The Prompts context.
   """
 
   alias SkepticBot.Podcasts.Episode
@@ -8,45 +8,48 @@ defmodule SkepticBot.Prompts do
   alias SkepticBot.Repo
 
   @type attrs :: map()
-  @type changeset :: Ecto.Changeset.t()
   @type embedding :: [float()]
-  @type episode :: map()
+  @type episode :: Episode.t()
+  @type episode_details :: map()
   @type id :: Ecto.UUID.t()
+  @type prompts_episode :: SkepticBot.Prompts.Episode.t()
   @type question :: UserQuestion.t()
-  @type question_episode :: map()
 
   @spec get_question(id()) :: question() | nil
   def get_question(id), do: Repo.get(UserQuestion, id)
 
-  @spec get_question_episodes([question_episode()]) :: [episode()]
+  @spec get_question_episodes([prompts_episode()]) :: [episode()]
   def get_question_episodes(question_episodes) do
-    Enum.reduce(question_episodes, [], fn episode, list_of_episodes ->
+    Enum.reduce(question_episodes, [], fn episode, podcast_episodes ->
       episode =
         Episode
         |> Repo.get!(episode.episode_id)
         |> Map.put(:timestamp, episode.timestamp)
 
-      [episode | list_of_episodes]
+      [episode | podcast_episodes]
     end)
   end
 
   @spec create_question(attrs()) ::
-          {:ok, question()} | {:error, changeset()}
-  def create_question(attrs) do
+          {:ok, question()} | {:error, Ecto.Changeset.t()}
+  def create_question(attrs \\ %{}) do
     %UserQuestion{}
     |> UserQuestion.changeset(attrs)
     |> Repo.insert()
   end
 
-  @spec change_prompt_question(question(), attrs()) :: changeset()
+  @spec change_prompt_question(question(), attrs()) :: Ecto.Changeset.t()
   def change_prompt_question(%UserQuestion{} = question, attrs \\ %{}) do
     UserQuestion.question_changeset(question, attrs)
   end
 
-  @spec get_episode_details([episode()]) :: [episode()]
-  def get_episode_details(list_of_episodes) do
-    Enum.reduce(list_of_episodes, [], fn episode, list ->
-      [%{episode_id: episode.id, timestamp: episode.timestamp} | list]
+  @spec get_episode_details([episode()]) :: [episode_details()]
+  def get_episode_details(podcast_episodes) do
+    Enum.reduce(podcast_episodes, [], fn podcast_episode, question_episodes ->
+      [
+        %{episode_id: podcast_episode.id, timestamp: podcast_episode.timestamp}
+        | question_episodes
+      ]
     end)
   end
 end
