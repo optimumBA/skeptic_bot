d0110d9e9766a8b1626982dd0c843d7a4e51e97a
Dean Kinyua <kinyuadean@gmail.com>
2025-06-27 14:06:14 +0300

    add new type messages in generator

--- lib/skeptic_bot/rag/generator.ex
+++ lib/skeptic_bot/rag/generator.ex
@@ -3,13 +3,13 @@ defmodule SkepticBot.Rag.Generator do
 
   alias SkepticBot.Rag.ReplicateGenerator
 
-  @type message :: [LangChain.Message.t()]
+  @type messages :: [LangChain.Message.t()]
   @type reason :: String.t()
   @type response :: String.t()
 
-  @callback predict(message()) :: {:ok, response()} | {:error, reason()}
+  @callback predict(messages()) :: {:ok, response()} | {:error, reason()}
 
-  @spec predict(message()) :: {:ok, response()} | {:error, reason()}
+  @spec predict(messages()) :: {:ok, response()} | {:error, reason()}
   def predict(messages), do: impl().predict(messages)
 
   defp impl, do: Application.get_env(:skeptic_bot, :generator, ReplicateGenerator)
