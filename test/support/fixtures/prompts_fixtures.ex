dd20e5bb9a2fa5d5f972a8ae618a4f93af199d85
Dean Kinyua <kinyuadean@gmail.com>
2025-06-30 11:32:33 +0300

    remove description fixture

--- test/support/fixtures/prompts_fixtures.ex
+++ test/support/fixtures/prompts_fixtures.ex
@@ -10,7 +10,6 @@ defmodule SkepticBot.PromptsFixtures do
   alias SkepticBot.Prompts
   alias SkepticBot.Prompts.UserQuestion
 
-  @type description :: String.t()
   @type episode :: Episode.t()
   @type question :: UserQuestion.t()
 
@@ -27,7 +26,7 @@ defmodule SkepticBot.PromptsFixtures do
 
     question_attrs =
       Enum.into(attrs, %{
-        description: description_fixture(),
+        description: "A simple description",
         embedding: Enum.map(1..1024, fn _some_random_float -> :rand.uniform() end),
         episodes: episode_details,
         query: "American Ponzi with Lee Camp"
@@ -37,19 +36,6 @@ defmodule SkepticBot.PromptsFixtures do
       Prompts.create_question(question_attrs)
 
     question
-  end
-
-  @doc """
-  creates a description.
-  """
-  @spec description_fixture :: description()
-  def description_fixture do
-    random_string =
-      12
-      |> :crypto.strong_rand_bytes()
-      |> Base.encode64()
-
-    "A random description #{random_string}"
   end
 
   @doc """
