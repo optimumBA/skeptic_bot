b1e1d7698dc674cdcd22044cde635e73c9e81e65
Dean Kinyua <kinyuadean@gmail.com>
2025-07-11 15:44:41 +0300

    Return only relevant episodes (#21)

--- lib/skeptic_bot/rag.ex
+++ lib/skeptic_bot/rag.ex
@@ -8,11 +8,26 @@ defmodule SkepticBot.Rag do
   alias SkepticBot.Podcasts
   alias SkepticBot.Rag
 
-  @spec generate(String.t()) :: {:ok, {String.t(), list()}} | {:error, any()}
+  @spec generate(String.t()) ::
+          {:ok, {String.t(), list()}} | {:error, any()} | :no_episodes_found
   def generate(query) do
-    with {:ok, [embedding]} <- Rag.Embedder.generate("query: " <> query),
-         context <- Rag.Retrieval.retrieve(embedding),
-         prompt <- format_prompt(context, query),
+    case Rag.Embedder.generate("query: " <> query) do
+      {:ok, [embedding]} ->
+        case Rag.Retrieval.retrieve(embedding) do
+          [] ->
+            :no_episodes_found
+
+          context ->
+            predict_query(context, query)
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp predict_query(context, query) do
+    with prompt <- format_prompt(context, query),
          {:ok, response} <- Rag.Generator.predict(prompt) do
       {:ok, {response, context}}
     else
@@ -37,11 +52,17 @@ defmodule SkepticBot.Rag do
         Message.new_user!(~s"""
         Context:
         --------------------------------------
-        #{Enum.map_join(context, "\n--------------------------------------\n", &format_episode/1)}
+        #{format_context(context)}
         --------------------------------------
         Query: #{query}
         """)
       ]
+  end
+
+  defp format_context(context) do
+    context
+    |> Enum.take(3)
+    |> Enum.map_join("\n--------------------------------------\n", &format_episode/1)
   end
 
   defp format_episode(%Podcasts.Episode{} = episode) do
