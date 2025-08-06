4f69efb40f61c49edeed31d61ac65c3fe519fb9f
Dean Kinyua <kinyuadean@gmail.com>
2025-08-06 16:22:53 +0300

    change size of images on homepage mobile

--- lib/skeptic_bot_web/live/home_live/index.ex
+++ lib/skeptic_bot_web/live/home_live/index.ex
@@ -8,43 +8,70 @@ defmodule SkepticBotWeb.HomeLive.Index do
 
   require Logger
 
+  #  <div class="bg-[#FFF5F5] border border-red-400">
+  #   <div class={[
+  #     "h-screen flex items-center relative",
+  #     @loading && "animate-pulse"
+  #   ]}>
+  # <section>
+  #   <div class="absolute top-[2%] left-0 w-[20%] 2xl:top-[3%] 4xl:w-[17%]">
+  #     <img src={~p"/images/home/top_swirl.svg"} class="w-full h-full object-cover" alt="Swirl" />
+  #   </div>
+  #   <div class="absolute bottom-[20%] left-[4%] w-[20%] xl:w-[21%] 2xl:w-[20%] 4xl:w-[17%]">
+  #     <img
+  #       src={~p"/images/home/demonstration.svg"}
+  #       class="w-full h-full object-cover"
+  #       alt="Illustration 1"
+  #     />
+  #   </div>
+  #   <div class="absolute bottom-[10%] right-[1.3rem] w-[10%]">
+  #     <img
+  #       src={~p"/images/home/hero_stars.svg"}
+  #       class="w-full h-full object-cover"
+  #       alt="Stars Group"
+  #     />
+  #   </div>
+  # </section>
+  #     <section class="flex flex-col gap-8 w-[70%] mx-auto">
+  #       <section class="text-7xl mx-auto montserrat-semibold tracking-4 2xl:text-8xl">
+  #         Skeptic.<span class="text-[#CD4631] montserrat-alternates-semibold">bot</span>
+  #       </section>
+  #       <section class="w-[50%] mx-auto text-center montserrat-alternates-medium text-[#4D4D4D]">
+  #         Questions everything
+  #       </section>
+  #       <section class="w-[60%] mx-auto">
+  #         <HomeLive.Components.form_component form={@form} />
+  #       </section>
+  #     </section>
+  #   </div>
+  # </div>
+
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div class="bg-[#FFF5F5]">
+    <div class="bg-[#FFF5F5] border border-red-400">
       <div class={[
         "h-screen flex items-center relative",
         @loading && "animate-pulse"
       ]}>
-        <section>
-          <div class="absolute top-[2%] left-0 w-[20%] 2xl:top-[3%] 4xl:w-[17%]">
+        <section class="pt-20 md:pt-0">
+          <div class="absolute top-[2%] left-0 w-[38%] md:w-[20%] 2xl:top-[3%] 4xl:w-[17%]">
             <img src={~p"/images/home/top_swirl.svg"} class="w-full h-full object-cover" alt="Swirl" />
           </div>
-          <div class="absolute bottom-[20%] left-[4%] w-[20%] xl:w-[21%] 2xl:w-[20%] 4xl:w-[17%]">
+          <div class="absolute bottom-[20%] left-[4%] w-[34%] md:w-[20%] xl:w-[21%] 2xl:w-[20%] 4xl:w-[17%]">
             <img
               src={~p"/images/home/demonstration.svg"}
               class="w-full h-full object-cover"
               alt="Illustration 1"
             />
           </div>
-          <div class="absolute bottom-[10%] right-[1.3rem] w-[10%]">
+          <div class="absolute bottom-[10%] right-[1.3rem] w-[24%] md:w-[10%]">
             <img
               src={~p"/images/home/hero_stars.svg"}
               class="w-full h-full object-cover"
               alt="Stars Group"
             />
           </div>
-        </section>
-        <section class="flex flex-col gap-8 w-[70%] mx-auto">
-          <section class="text-7xl mx-auto montserrat-semibold tracking-4 2xl:text-8xl">
-            Skeptic.<span class="text-[#CD4631] montserrat-alternates-semibold">bot</span>
-          </section>
-          <section class="w-[50%] mx-auto text-center montserrat-alternates-medium text-[#4D4D4D]">
-            Questions everything
-          </section>
-          <section class="w-[60%] mx-auto">
-            <HomeLive.Components.form_component form={@form} />
-          </section>
         </section>
       </div>
     </div>
