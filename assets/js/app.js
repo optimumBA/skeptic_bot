f5c41a23ecc2a7728bc7b3d29bedfbd731edd89d
Dean Kinyua <kinyuadean@gmail.com>
2025-08-14 15:29:08 +0300

    remove typewriter hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,17 +21,11 @@ import 'phoenix_html'
 import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import topbar from '../vendor/topbar'
-import TypewriterHooks from './hooks/typewriter'
-
-let Hooks = {
-  ...TypewriterHooks,
-}
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content')
 let liveSocket = new LiveSocket('/live', Socket, {
-  hooks: Hooks,
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
 })
