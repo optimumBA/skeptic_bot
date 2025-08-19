5cf7b5941f46f9afa01b432e419a9b2b964d080d
Dean Kinyua <kinyuadean@gmail.com>
2025-08-19 16:56:34 +0300

    increase the timeout even more

--- lib/skeptic_bot/podcasts/downloading_worker.ex
+++ lib/skeptic_bot/podcasts/downloading_worker.ex
@@ -44,7 +44,7 @@ defmodule SkepticBot.Podcasts.DownloadingWorker do
       FLAME.call(
         DownloadingRunner,
         fn -> process(id, video_url, podcast) end,
-        timeout: 3_400_000
+        timeout: 4_400_000
       )
 
     case result do
