c69807f884cc8ce9cf1611c3ca28538b8be3140d
Dean Kinyua <kinyuadean@gmail.com>
2025-08-27 09:17:42 +0300

    scrape only 2

--- lib/skeptic_bot/look_into_it/scraper.ex
+++ lib/skeptic_bot/look_into_it/scraper.ex
@@ -24,6 +24,7 @@ defmodule SkepticBot.LookIntoIt.Scraper do
 
         result
         |> format_channel_data()
+        |> Enum.take(-2)
         |> Enum.each(&maybe_download_episode(&1, channel, podcast.id))
 
       {:error, reason} ->
