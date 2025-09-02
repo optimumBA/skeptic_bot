403dbde9daaa4e1345d9109c633cb801b77a7a65
Dean Kinyua <kinyuadean@gmail.com>
2025-09-02 15:22:13 +0300

    disable scraping and revert scraping worker module

--- lib/skeptic_bot/podcasts/scraping_worker.ex
+++ lib/skeptic_bot/podcasts/scraping_worker.ex
@@ -8,9 +8,8 @@ defmodule SkepticBot.Podcasts.ScrapingWorker do
     max_attempts: 3,
     queue: :scraping
 
-  # alias SkepticBot.Podcasts.TinfoilScraper
-  # alias SkepticBot.YtDlp.CandaceServer
-  # alias SkepticBot.YtDlp.Scraper
+  alias SkepticBot.LookIntoIt.Scraper
+  alias SkepticBot.Podcasts.TinfoilScraper
 
   require Logger
 
@@ -20,8 +19,8 @@ defmodule SkepticBot.Podcasts.ScrapingWorker do
   @spec perform(job()) :: :ok | {:error, any()}
   def perform(_job) do
     Logger.info("Starting scheduled podcast scraping")
-    # TinfoilScraper.scrape()
-    # Scraper.scrape()
-    # CandaceServer.request_episodes()
+    TinfoilScraper.scrape()
+    Scraper.scrape()
+    SkepticBot.Candace.Scraper.scrape()
   end
 end
