41ef6b4b4000f970c2bc2858307f2b1d8aed1f9b
Dean Kinyua <kinyuadean@gmail.com>
2025-09-18 09:37:24 +0300

    scrape only two

--- priv/download_deep_waters.exs
+++ priv/download_deep_waters.exs
@@ -33,7 +33,7 @@ podcast = Podcasts.get_podcast_by_name("Deep Waters")
 
 path
 |> get_episodes.()
-|> Enum.take(-2)
+|> Enum.take(-3)
 |> Enum.each(
   &EpisodeProcessor.maybe_download_episode(
     &1,
