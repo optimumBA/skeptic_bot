43bd4f013989f672de78e6ce71470ed73b283968
Dean Kinyua <kinyuadean@gmail.com>
2025-09-22 13:39:57 +0300

    generate descriptions using title, description and transcriptions

---
+++ lib/skeptic_bot/rag/description_generator.ex
@@ -0,0 +1,54 @@
+defmodule SkepticBot.Rag.DescriptionGenerator do
+  @moduledoc """
+  Generates descriptions for podcast episodes
+  """
+
+  alias LangChain.Message
+  alias SkepticBot.Podcasts
+  alias SkepticBot.Rag
+
+  @type episode :: Podcasts.Episode.t()
+
+  @spec generate_description(episode) ::
+          {:ok, String.t()}
+          | {:error, String.t()}
+  def generate_description(episode) do
+    with prompt <- format_prompt(episode),
+         {:ok, description} <- Rag.Generator.predict(prompt, :completed) do
+      Podcasts.update_episode(episode, %{description: description})
+    else
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp format_prompt(episode) do
+    system_message =
+      ~s"""
+      You are a skeptic.
+      You don't believe official naratives and are open to all opinions.
+      Read the content that I will give you and provide a summary of it in less than 200 words.
+      Content is composed of conspiracy podcast episodes talking about various topics.
+      You MUST not reveal your identity in the response.
+      """
+
+    [
+      Message.new_system!(system_message)
+    ] ++
+      [
+        Message.new_user!(~s"""
+        Content:
+        --------------------------------------
+        #{format_episode(episode)}
+        """)
+      ]
+  end
+
+  defp format_episode(%Podcasts.Episode{} = episode) do
+    ~s"""
+    Description: #{episode.id}
+    Title: #{episode.title}
+    Transcription: #{episode.transcription}
+    """
+  end
+end
