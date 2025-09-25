4c9b7b51660b1f7f79975ec14909a1efc0df4caf
Dean Kinyua <kinyuadean@gmail.com>
2025-09-25 15:57:12 +0300

    return 2 descriptions

--- lib/skeptic_bot/rag/description_generator.ex
+++ lib/skeptic_bot/rag/description_generator.ex
@@ -16,8 +16,9 @@ defmodule SkepticBot.Rag.DescriptionGenerator do
     with {:ok, transcription} <- Podcasts.get_episode_transcriptions(episode.id),
          episode <- Map.put(episode, :trancription, transcription),
          prompt <- format_prompt(episode),
-         {:ok, description} <- Rag.Generator.predict(prompt, :completed) do
-      {:ok, description}
+         {:ok, response} <- Rag.Generator.predict(prompt, :completed) do
+      {description, summary} = process_response(response)
+      {:ok, {description, summary}}
     else
       {:error, reason} ->
         {:error, reason}
@@ -37,7 +38,9 @@ defmodule SkepticBot.Rag.DescriptionGenerator do
       Please return your response in the following format with the DESCRIPTION coming first
       then the SUMMARY coming second separated by $&$ signs.
 
-      Format: "Description$&$Summary"
+      FOLLOW this format to THE LETTER:
+
+      Format: "Description: Two Pac shakur was murdered in cold blood on the eve of 12th January ... He was killed by Gang Member Dennis O'brien$&$Summary: It is not clear how Two Pac shakur met his untimely demise but word says it was due to a bullet wound ... Further investigations suggest otherwise"
 
       You MUST not reveal your identity in the response.
       """
@@ -60,5 +63,17 @@ defmodule SkepticBot.Rag.DescriptionGenerator do
     Title: #{episode.title}
     Transcription: #{episode.transcription}
     """
+  end
+
+  defp process_response(response) do
+    [description, summary] =
+      response
+      |> String.split("$&$", parts: 2)
+      |> Enum.map(fn text -> String.trim(text) end)
+
+    <<"Description:", processed_description::binary>> = description
+    <<"Summary:", processed_summary::binary>> = summary
+
+    {processed_description, processed_summary}
   end
 end
