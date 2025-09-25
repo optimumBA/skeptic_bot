b16d18aa19e0da8954166f094d2bf21532e3741a
Dean Kinyua <kinyuadean@gmail.com>
2025-09-25 13:07:46 +0300

    change tests

--- test/skeptic_bot_web/live/question_live_test.exs
+++ test/skeptic_bot_web/live/question_live_test.exs
@@ -8,8 +8,6 @@ defmodule SkepticBotWeb.QuestionLiveTest do
   defp create_question(%{conn: conn}) do
     embedding = embedding_fixture()
 
-    create_multiple_episodes(2, embedding)
-
     question = question_fixture(embedding: embedding)
 
     %{conn: conn, embedding: embedding, question: question}
@@ -54,10 +52,11 @@ defmodule SkepticBotWeb.QuestionLiveTest do
       assert html =~ "00:20:00"
     end
 
-    test "uses different webpage_urls based on the podcast", %{
-      conn: conn,
-      embedding: embedding
-    } do
+    test "uses different webpage_urls based on the podcast (\"Tin Foil Hat\", \"Look Into It\", \"Candace\", \"Cash Daddies\", \"Doom Scrollin\")",
+         %{
+           conn: conn,
+           embedding: embedding
+         } do
       episode =
         episode_fixture(
           %{
@@ -103,6 +102,130 @@ defmodule SkepticBotWeb.QuestionLiveTest do
           "Candace"
         )
 
+      episode_5 =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id: "fa3z9PEPSKzHejBSoVGPvn"
+          },
+          "Cash Daddies"
+        )
+
+      episode_6 =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id: "na4z9EERSKzHedBSoVGPvn"
+          },
+          "Doom Scrollin"
+        )
+
+      question =
+        question_fixture(
+          embedding: embedding,
+          episodes: [
+            %{episode_id: episode.id, timestamp: %{secs: 45, months: 0, days: 0}},
+            %{episode_id: episode_2.id, timestamp: %{secs: 30, months: 0, days: 0}},
+            %{episode_id: episode_3.id, timestamp: %{secs: 20, months: 0, days: 0}},
+            %{episode_id: episode_4.id, timestamp: %{secs: 76, months: 0, days: 0}},
+            %{episode_id: episode_5.id, timestamp: %{secs: 76, months: 0, days: 0}},
+            %{episode_id: episode_6.id, timestamp: %{secs: 76, months: 0, days: 0}}
+          ],
+          title: "American Ponzi with Lee Camp and Sam Tripoli"
+        )
+
+      {:ok, _view, html} = live(conn, ~p"/questions/#{question.id}")
+
+      webpage_url = "https://vid.samtripoli.com/w/na3z9PEPSKzHedBSoVGPvn?start=45"
+
+      webpage_url_2 =
+        "https://rumble.com/v51g8i8-alex-jones-on-look-into-it-with-eddie-bravo-episode-101.html?e9s=src_v1_ucp_a&amp;start=30"
+
+      webpage_url_3 = "https://rokfin.com/post/145321?start=20"
+      webpage_url_4 = "https://www.youtube.com/watch?v=DoRzUmsF800?start=76"
+
+      assert html =~ webpage_url
+      assert html =~ webpage_url_2
+      assert html =~ webpage_url_3
+      assert html =~ webpage_url_4
+    end
+
+    test "uses different webpage_urls based on the podcast (\"Cash Daddies\", \"DoomScrollin\", \"Broken Simulation\")",
+         %{
+           conn: conn,
+           embedding: embedding
+         } do
+      episode =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id: "na3z9PEPSKzHedBSoVGPvn"
+          },
+          "Tin Foil Hat"
+        )
+
+      episode_2 =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id:
+              "v51g8i8-alex-jones-on-look-into-it-with-eddie-bravo-episode-101.html?e9s=src_v1_ucp_a"
+          },
+          "Look Into It"
+        )
+
+      episode_3 =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id: "145321"
+          },
+          "Look Into It"
+        )
+
+      episode_4 =
+        episode_fixture(
+          %{
+            episode_length: 3000,
+            title: "Consistency truly is key to mastering any skill over time and effort",
+            embedding: embedding,
+            external_id: "DoRzUmsF800"
+          },
+          "Candace"
+        )
+
+      # episode_5 =
+      #   episode_fixture(
+      #     %{
+      #       episode_length: 3000,
+      #       title: "Consistency truly is key to mastering any skill over time and effort",
+      #       embedding: embedding,
+      #       external_id: "DoRzUmsF800"
+      #     },
+      #     "Candace"
+      #   )
+
+      # episode_6 =
+      #   episode_fixture(
+      #     %{
+      #       episode_length: 3000,
+      #       title: "Consistency truly is key to mastering any skill over time and effort",
+      #       embedding: embedding,
+      #       external_id: "DoRzUmsF800"
+      #     },
+      #     "Candace"
+      #   )
+
       question =
         question_fixture(
           embedding: embedding,
@@ -111,6 +234,8 @@ defmodule SkepticBotWeb.QuestionLiveTest do
             %{episode_id: episode_2.id, timestamp: %{secs: 30, months: 0, days: 0}},
             %{episode_id: episode_3.id, timestamp: %{secs: 20, months: 0, days: 0}},
             %{episode_id: episode_4.id, timestamp: %{secs: 76, months: 0, days: 0}}
+            # %{episode_id: episode_5.id, timestamp: %{secs: 76, months: 0, days: 0}},
+            # %{episode_id: episode_6.id, timestamp: %{secs: 76, months: 0, days: 0}}
           ],
           title: "American Ponzi with Lee Camp and Sam Tripoli"
         )
@@ -133,8 +258,11 @@ defmodule SkepticBotWeb.QuestionLiveTest do
 
     test "displays the related questions", %{
       conn: conn,
+      embedding: embedding,
       question: question
     } do
+      create_multiple_episodes(2, embedding)
+
       embedding = Pgvector.to_list(question.embedding)
 
       question_fixture(
@@ -156,8 +284,10 @@ defmodule SkepticBotWeb.QuestionLiveTest do
 
     test "updates after completing response", %{
       conn: conn,
+      embedding: embedding,
       question: question
     } do
+      create_multiple_episodes(2, embedding)
       {:ok, view, _html} = live(conn, ~p"/questions/#{question.id}")
 
       send(view.pid, {:prediction_result, {"New Title", "New Description"}})
@@ -170,8 +300,10 @@ defmodule SkepticBotWeb.QuestionLiveTest do
     test "removes the loader after completing response",
          %{
            conn: conn,
+           embedding: embedding,
            question: question
          } do
+      create_multiple_episodes(2, embedding)
       {:ok, view, _html} = live(conn, ~p"/questions/#{question.id}")
 
       send(view.pid, {:prediction_complete, {"New Title", "New Description"}})
