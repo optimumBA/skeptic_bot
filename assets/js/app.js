9536225caf42744f253957065d4fc1fdede1c603
Almir Sarajčić <almir.sarajcic@icloud.com>
2025-10-01 10:27:49 +0200

    Remove colocated hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,6 @@ import 'phoenix_html'
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
-import { hooks as colocatedHooks } from 'phoenix-colocated/skeptic_bot'
 import topbar from '../vendor/topbar'
 
 const csrfToken = document
@@ -32,7 +31,6 @@ const csrfToken = document
 const liveSocket = new LiveSocket('/live', Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: { ...colocatedHooks },
 })
 
 // Show progress bar on live navigation and form submits
