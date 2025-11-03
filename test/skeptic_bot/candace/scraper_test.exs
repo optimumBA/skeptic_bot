73b27c329a4b855971176ef37b03074e7b1854c6
Dean Kinyua <kinyuadean@gmail.com>
2025-11-03 15:45:29 +0300

    fix tests

--- test/skeptic_bot/candace/scraper_test.exs
+++ test/skeptic_bot/candace/scraper_test.exs
@@ -22,6 +22,7 @@ defmodule SkepticBot.Candace.ScraperTest do
         "Eminem Drops A Diss Track Ep 24~~2500~~https://i.ytimg.com/vi/3CHfault.jpg~~https://www.youtube.com/watch?v=3CHHx4pkBIo"
 
       port = Port.open({:spawn, "echo #{msg}"}, [:binary])
+      port_2 = Port.open({:spawn, "echo #{msg}"}, [:binary])
 
       msg_2 =
         "Eminem Drops A Diss Track Ep 25~~2500~~https://i.ytimg.com/vi/3CHfault.jpg~~https://www.youtube.com/watch?v=3CHHx4pkBI4"
@@ -29,9 +30,14 @@ defmodule SkepticBot.Candace.ScraperTest do
       send(self(), {port, {:data, msg_2}})
 
       Process.send_after(self(), {:close_port, port}, 100)
+      Process.send_after(self(), {:close_port, port_2}, 100)
 
       expect(MockChannelClient, :get_channel_data_from_port, fn _date, _channel ->
         port
+      end)
+
+      expect(MockChannelClient, :get_channel_data_from_port, fn _date, _channel ->
+        port_2
       end)
 
       Scraper.scrape()
@@ -60,11 +66,17 @@ defmodule SkepticBot.Candace.ScraperTest do
         "Warning! Please sign in to confirm your age."
 
       port = Port.open({:spawn, "echo #{msg}"}, [:binary])
+      port_2 = Port.open({:spawn, "echo #{msg}"}, [:binary])
 
       Process.send_after(self(), {:close_port, port}, 100)
+      Process.send_after(self(), {:close_port, port_2}, 100)
 
       expect(MockChannelClient, :get_channel_data_from_port, fn _date, _channel ->
         port
+      end)
+
+      expect(MockChannelClient, :get_channel_data_from_port, fn _date, _channel ->
+        port_2
       end)
 
       log =
