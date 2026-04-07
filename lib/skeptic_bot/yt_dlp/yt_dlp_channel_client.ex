cc2d2b10c5197cb48593a6d6374cbe0ae317d578
Almir Sarajčić <almir.sarajcic@icloud.com>
2026-04-07 08:52:41 +0200

    Fix syntax error

--- lib/skeptic_bot/yt_dlp/yt_dlp_channel_client.ex
+++ lib/skeptic_bot/yt_dlp/yt_dlp_channel_client.ex
@@ -34,7 +34,7 @@ defmodule SkepticBot.YtDlp.YtDlpChannelClient do
     cookie_file = Application.get_env(:skeptic_bot, :youtube_cookie_file_path)
 
     cmd =
-      "PYTHONUTF8=1 yt-dlp --cache-dir #{System.tmp_dir!()} --date #{date} --cookies #{cookie_file} --print \"%(title)s~~%(duration)s~~%(thumbnail)s~~%(webpage_url)s\" #{channel}"
+      "env PYTHONUTF8=1 yt-dlp --cache-dir #{System.tmp_dir!()} --date #{date} --cookies #{cookie_file} --print \"%(title)s~~%(duration)s~~%(thumbnail)s~~%(webpage_url)s\" #{channel}"
 
     port = Port.open({:spawn, cmd}, [:binary, :stderr_to_stdout, :exit_status])
     Process.send_after(self(), {:close_port, port}, :timer.minutes(1))
